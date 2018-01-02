83ec7059f7c345f1929764faf335eb87eda2f182
JTron814 <joshbors14@gmail.com>
2018-01-01 23:57:39 -0600

    added create user layout

--- app/src/main/java/com/example/joshb/smack/MainActivity.kt
+++ app/src/main/java/com/example/joshb/smack/MainActivity.kt
@@ -46,14 +46,4 @@ class MainActivity : AppCompatActivity() {
     fun sendMessageBtnClicked(view: View) {
 
     }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        when (item.itemId) {
-            R.id.action_settings -> return true
-            else -> return super.onOptionsItemSelected(item)
-        }
-    }
 }
